3a44ce3ea68df9e1737005db928c99503e6230c3
urakaterinenko <yorikyorik25@gmail.com>
2023-05-26 21:49:40 +0300

    reading data from a json file

--- data/script.py
+++ data/script.py
@@ -1,10 +1,13 @@
 data = '''['-1',null,[[['ANDROID_BACKUP',0],['BATTERY_STATS',0],['SMART_SETUP',0],['TRON',0]],-3334737594024971225],[],{'175237375':[10000]}] '''
 
-python_data = ['-1', None,
-               [[['ANDROID_BACKUP', 0], ['BATTERY_STATS', 0], ['SMART_SETUP', 0], ['TRON', 0]], -3334737594024971225],
-               [], {'175237375': [10000]}]
+
 
 import json
 
-with open('data.json', 'w') as f:
-    json.dump(python_data, f)
+data = []
+
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+
+print(data)
